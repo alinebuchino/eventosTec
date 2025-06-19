824fd42d4928f2ec3566f55555b43dd4d1b562f9
Aline Buchino <aline.buchino@hotmail.com>
2025-06-19 17:05:59 -0300

    feat: first commit

---
+++ src/test/java/com/alinebuchino/eventosTec/EventosTecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alinebuchino.eventosTec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventosTecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
