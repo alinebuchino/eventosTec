ea2dd20da7eb992fd637c24fcc75af5b1e303461
Aline Buchino <aline.buchino@hotmail.com>
2025-06-21 15:33:06 -0300

    feat: configurações AWS e chamada para envio arquivo no S3

--- src/main/java/com/alinebuchino/eventosTec/services/EventService.java
+++ src/main/java/com/alinebuchino/eventosTec/services/EventService.java
@@ -2,13 +2,29 @@ package com.alinebuchino.eventosTec.services;
 
 import com.alinebuchino.eventosTec.DTOs.EventRequestDTO;
 import com.alinebuchino.eventosTec.domain.Event;
+import com.amazonaws.services.s3.AmazonS3;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Date;
+import java.util.Objects;
+import java.util.UUID;
 
 @Service
 public class EventService {
+
+    @Autowired
+    private AmazonS3 s3cliente;
+
+    @Value("${aws.bucket.name}")
+    private String awsBucketName;
+
     public Event createEvent(EventRequestDTO data) {
         String imgUrl = null;
 
@@ -27,7 +43,26 @@ public class EventService {
         return newEvent;
     }
 
-    private String uploadImg(MultipartFile file) {
-        return "";
+    private String uploadImg(MultipartFile multipartFile) {
+        String fileName = UUID.randomUUID() + "-" + multipartFile.getOriginalFilename();
+
+        try {
+            File file = this.convertMultipartToFile(multipartFile);
+            s3cliente.putObject(awsBucketName, fileName, file);
+            file.delete();
+
+            return s3cliente.getUrl(awsBucketName, fileName).toString();
+        } catch (Exception e) {
+            System.out.println("Erro ao enviar o arquivo na AWS");
+            return null;
+        }
+    }
+
+    private File convertMultipartToFile(MultipartFile multipartFile) throws IOException {
+        File convFile = new File(Objects.requireNonNull(multipartFile.getOriginalFilename()));
+        FileOutputStream fos = new FileOutputStream(convFile);
+        fos.write(multipartFile.getBytes());
+        fos.close();
+        return convFile;
     }
 }
